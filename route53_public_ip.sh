c0a7a26c6d083cf56ab8d59229a1ee5b65ada055
Irfan Gulamali <irfan.412@yahoo.com>
2020-08-11 19:34:51 -0400

    refactor

--- route53_public_ip.sh
+++ route53_public_ip.sh
@@ -1,9 +1,12 @@
-#! /bin/bash
+#! /bin/sh
+
+# grab host zones
+# aws route53 list-hosted-zones
 
 ZONE_ID=''
 HOST_RECORD=''
 PUB_IP=$(curl -s https://checkip.amazonaws.com)
 
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch \
+python -m awscli route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch \
 '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$HOST_RECORD'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PUB_IP'"}]}}]}'
 
